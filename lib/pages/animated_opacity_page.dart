2351ee5e436670e5a5fb3437f437005d2a9279e8
Temidayo <temideewan@gmail.com>
2022-08-24 11:49:06 +0100

    animation basics introduction

---
+++ lib/pages/animated_opacity_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOpacityPage extends StatefulWidget {
+  @override
+  createState() => AnimatedOpacityPageState();
+}
+
+class AnimatedOpacityPageState extends State<AnimatedOpacityPage> {
+  double _opacity = 1.0;
+
+  void _toggleFade() {
+    setState(() {
+      _opacity = _opacity == 1.0 ? 0.0 : 1.0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedOpacity'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn,
+              child: FlutterLogo(size: 200),
+            ),
+            ElevatedButton(
+              child: Text('Fade Logo'),
+              // TODO: Implement
+              onPressed: _toggleFade,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/tween_animation_builder_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class TweenAnimationBuilderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TweenAnimationBuilder'),
+      ),
+      body: Center(
+        child: HSVColorSelector(),
+      ),
+    );
+  }
+}
+
+class HSVColorSelector extends StatefulWidget {
+  @override
+  _HSVColorSelectorState createState() => _HSVColorSelectorState();
+}
+
+class _HSVColorSelectorState extends State<HSVColorSelector> {
+  double _hue = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedContainer(
+          duration: Duration(milliseconds: 1500),
+          width: _hue,
+          height: 200,
+          color: HSVColor.fromAHSV(1.0, _hue, 1.0, 1.0).toColor(),
+        ),
+        SizedBox(height: 40.0),
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 0.0, end: _hue),
+          curve: Curves.easeInCubic,
+          duration: Duration(milliseconds: 1500),
+          builder: (context, hue, child) {
+            final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
+            return Container(
+                width: _hue, height: 200.0, color: hsvColor.toColor());
+          },
+        ),
+        SizedBox(height: 48.0),
+        Container(
+          height: 30.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              for (var h = 0; h <= 360; h++)
+                HSVColor.fromAHSV(1.0, h.toDouble(), 1.0, 1.0).toColor()
+            ], stops: [
+              for (var h = 0; h <= 360; h++) h.toDouble() / 360
+            ]),
+          ),
+        ),
+        Slider.adaptive(
+          value: _hue,
+          onChanged: (value) => setState(() => _hue = value),
+          min: 0,
+          max: 360,
+        )
+      ],
+    );
+  }
+}
